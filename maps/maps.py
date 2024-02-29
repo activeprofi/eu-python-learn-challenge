ab48bbae6950e4941e2c8da5ec2c225a6bd43925
Active Profi <atosdo@live.com>
2024-02-29 22:17:08 +0000

    Add solutions to map problems

--- maps/maps.py
+++ maps/maps.py
@@ -13,7 +13,19 @@ class MapExercise:
         Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
         :return: Средний рейтинг фильмов у которых две или больше стран
         """
-        pass
+
+        filtered_movies = filter(lambda m: len(m["country"].split(",")) >= 2, list_of_movies)
+
+        def map_func(m: dict) -> Union[float, None]:
+            rat = float(m["rating_kinopoisk"]) if m["rating_kinopoisk"] else None
+            return rat if rat and rat > 0 else None
+
+        ratings = map(map_func, filtered_movies)
+        filtered_ratings = list(filter(lambda r: r is not None, ratings))
+        ratings_sum = sum(filtered_ratings)
+        average_rat = ratings_sum / len(filtered_ratings)
+
+        return average_rat
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
@@ -28,4 +40,12 @@ class MapExercise:
         :return: Количество букв 'и' в названиях всех фильмов с рейтингом больше
         или равным заданному значению
         """
-        pass
+
+        def map_func(m: dict) -> Union[str, None]:
+            rat = float(m["rating_kinopoisk"]) if m["rating_kinopoisk"] else None
+            return m["name"].count("и") if rat and rat >= rating else None
+
+        counts = map(map_func, list_of_movies)
+        filtered_counts = filter(lambda n: n is not None, counts)
+
+        return sum(filtered_counts)
